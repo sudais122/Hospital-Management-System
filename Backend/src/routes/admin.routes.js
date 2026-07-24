08f03fd752729c8706a0cd9d508399a2403a2b9b
sudais122 <khansb17798@gmail.com>
2026-07-24 22:31:56 +0500

    admin contoller complete

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -5,10 +5,11 @@ import {
   getAllAppointments,
   adminLogin
 } from "../controllers/admin.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.get("/getAllDoctor", getAllDoctor);
-router.get("/getAllNurses", getAllNurses);
+router.get("/getAllDoctor",verifyJWT, getAllDoctor);
+router.get("/getAllNurses", verifyJWT,getAllNurses);
 router.post("/login", adminLogin);
-router.get("/getAllAppointments", getAllAppointments);
+router.get("/getAllAppointments", verifyJWT,getAllAppointments);
 export default router;
